95826d6094e447b1c3c2e01ad00e8da379d43ced
Amir <amir.mofakhar@birdstep.com>
2015-11-04 13:00:43 +0100

    adding cellinfo

--- app/src/main/java/info/mofakhar/infocell/MainActivity.java
+++ app/src/main/java/info/mofakhar/infocell/MainActivity.java
@@ -1,7 +1,10 @@
 package info.mofakhar.infocell;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,7 +17,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View v){
-        TextView tv4 = (TextView)findViewById(R.id.textView4);
-        tv4.setText("1");
+
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager.getCellLocation();
+
+        int cid = cellLocation.getCid();
+        int lac = cellLocation.getLac();
+
+        String cid_str = String.valueOf(cid);
+
+        TextView cell_1 = (TextView)findViewById(R.id.textView4);
+        cell_1.setText(cid_str);
+        TextView cell_2 = (TextView)findViewById(R.id.textView5);
+        cell_2.setText("1");
+        TextView cell_3 = (TextView)findViewById(R.id.textView6);
+        cell_3.setText("1");
+
     }
 }
