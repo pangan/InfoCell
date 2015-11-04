ca5d62d97f8cc21e836f46f3bdeb1d67a8a41c80
Amir <amir.mofakhar@birdstep.com>
2015-11-04 13:39:05 +0100

    adding cell type

--- app/src/main/java/info/mofakhar/infocell/MainActivity.java
+++ app/src/main/java/info/mofakhar/infocell/MainActivity.java
@@ -21,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager.getCellLocation();
 
-        int cid = cellLocation.getCid();
-        int lac = cellLocation.getLac();
-        int psc = cellLocation.getPsc();
+        int cid = cellLocation.getCid() & 0xffff;
+        int lac = cellLocation.getLac() & 0xffff;
+        int psc = cellLocation.getPsc() & 0xffff;
 
 
         String cid_str = String.valueOf(cid);
@@ -36,6 +36,64 @@ public class MainActivity extends AppCompatActivity {
         cell_2.setText(lac_str);
         TextView cell_3 = (TextView)findViewById(R.id.textView6);
         cell_3.setText(psc_str);
+
+        TextView textV1 =  (TextView)findViewById(R.id.textView7);
+
+        TelephonyManager teleMan =
+                (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+
+        int networkType = teleMan.getNetworkType();
+        switch (networkType)
+        {
+            case 7:
+                textV1.setText("1xRTT");
+                break;
+            case 4:
+                textV1.setText("CDMA");
+                break;
+            case 2:
+                textV1.setText("EDGE");
+                break;
+            case 14:
+                textV1.setText("eHRPD");
+                break;
+            case 5:
+                textV1.setText("EVDO rev. 0");
+                break;
+            case 6:
+                textV1.setText("EVDO rev. A");
+                break;
+            case 12:
+                textV1.setText("EVDO rev. B");
+                break;
+            case 1:
+                textV1.setText("GPRS");
+                break;
+            case 8:
+                textV1.setText("HSDPA");
+                break;
+            case 10:
+                textV1.setText("HSPA");
+                break;
+            case 15:
+                textV1.setText("HSPA+");
+                break;
+            case 9:
+                textV1.setText("HSUPA");
+                break;
+            case 11:
+                textV1.setText("iDen");
+                break;
+            case 13:
+                textV1.setText("LTE");
+                break;
+            case 3:
+                textV1.setText("UMTS");
+                break;
+            case 0:
+                textV1.setText("Unknown");
+                break;
+        }
 
     }
 }
